f4c2168f3ceeadbf18d33fa27dcc9a6140e48507
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-03-08 13:50:54 -0500

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,8 +1,16 @@
-export function managePresents(state = {
-    numberOfPresents: 0
-}, action){
-    switch (action.type){
-       case "INCREASE":
-        numberOfPresents++
-    }
+export function managePresents(
+  state = {
+    numberOfPresents: 0,
+  },
+  action
+) {
+  switch (action.type) {
+    case "INCREASE":
+      return Object.assign({}, state, {
+        numberOfPresents: state.numberOfPresents + 1,
+      });
+
+    default:
+      return state;
+  }
 }
